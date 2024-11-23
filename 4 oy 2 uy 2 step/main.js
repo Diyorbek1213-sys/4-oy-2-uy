ded59dbb261918b490e4597c6eddaf48f0924ca2
Diyorbek1213-sys <birorbirorkim@gmail.com>
2024-11-23 13:11:47 +0500

    4 oy 2 uy

---
+++ 4 oy 2 uy 2 step/main.js
@@ -0,0 +1,207 @@
+alert ("21-vazifa")
+// 21 chi vazifa
+let num_1 = +prompt ("son-1")
+let num_2 = +prompt ("son-2")
+
+if (num_1 === num_2) {
+    console.log(1)
+}
+
+else {
+    console.log(0)
+}
+
+alert ("22-vazifa")
+// 22 chi vazifa
+let number_1 = +prompt ("son-1")
+let number_2 = +prompt ("son-2")
+
+if (number_1 > number_2) {
+    console.log(2)
+}
+
+else {
+    console.log(3)
+}
+
+alert ("23-vazifa")
+// 23 chi vazifa
+let number_3 = +prompt ("son-1")
+let number_4 = +prompt ("son-2")
+
+if (number_3 < number_4 > 0) {
+    console.log(10)
+}
+
+alert ("24-vazifa")
+// 24 chi vazifa
+let number_5 = +prompt ("son-1")
+let number_6 = +prompt ("son-2")
+
+if (number_5 > number_6 % 2 == 1) {
+    console.log(5)
+}
+
+alert ("25-vazifa")
+// 25 chi vazifa
+let number_7 = +prompt ("son-1")
+let number_8 = +prompt ("son-2")
+
+if (number_7 && number_8 > 0) {
+    console.log(number_7 + number_8)
+} 
+
+alert ("26-vazifa")
+// 26 chi vazifa
+let number_9 = +prompt ("son-1")
+let number_10 = +prompt ("son-2")
+
+if (number_9 < number_10) {
+    if (number_10 % 2 == 0) {
+        console.log(-1)
+    }
+}
+
+alert ("27-vazifa")
+// 27 chi vazifa
+let number_11 = +prompt ("son-1")
+let number_12 = +prompt ("son-2")
+
+if (number_11 === number_12) {
+    console.log(100)
+}
+
+else {
+    console.log(-100)
+}
+
+alert ("28-vazifa")
+// 28 chi vazifa
+let number_13 = +prompt ("son-1")
+let number_14 = +prompt ("son-2")
+
+if (number_13 /= 10 == 0) {
+    if (number_14 /= 5 == 0) {
+        console.log(50)
+    }
+}
+
+alert ("29-vazifa")
+// 29 chi vazifa
+let number_15 = +prompt ("son-1")
+let number_16 = +prompt ("son-2")
+
+if (number_15 === 2 * number_16) {
+    console.log(2)
+}
+
+alert ("30-vazifa")
+// 30 chi vazifa
+let number_17 = +prompt ("son-1")
+let number_18 = +prompt ("son-2")
+
+if (number_17 < number_18) {
+    if (number_18 % 2 == 1) {
+        console.log(-10)
+    }
+}
+
+alert ("31-vazifa")
+// 31 chi vazifa
+let number_19 = +prompt ("son-1")
+
+if (number_19 > 0) {
+    if (number_19 % 3 == 0) {
+        console.log(3)
+    }
+
+    if (number_19 % 3 == 1) {
+        console.log(0)
+    }
+}
+
+alert ("32-vazifa")
+// 32 chi vazifa
+let number_20 = +prompt ("son-1")
+
+if (number_20 < 0) {
+    if (number_20 % 2 == 0) {
+        console.log(-2)
+    }
+}
+
+alert ("33-vazifa")
+// 33 chi vazifa
+let number_21 = +prompt ("son-1")
+
+if (number_21 > 0) {
+    console.log(number_21 % 5)
+}
+
+alert ("34-vazifa")
+// 34 chi vazifa
+let number_22 = +prompt ("son-1")
+
+if (number_22 > 0) {
+    if (number_22 < 10) {
+        console.log(number_22 * number_22)
+    }
+}
+
+alert ("35-vazifa")
+// 35 chi vazifa
+let number_23 = +prompt ("son-1")
+
+if (number_23 % 2 == 0 || number_23 % 4 == 0) {
+    console.log(1)
+}
+
+alert ("36-vazifa")
+// 36 chi vazifa
+let number_24 = +prompt ("son-1")
+
+if (number_24 % 2 == 1) {
+    if (number_24 > 15) {
+        console.log(15)
+    }
+}
+
+alert ("37-vazifa")
+// 37 chi vazifa
+let number_25 = +prompt ("son-1")
+
+if (number_25 % 2 === 0 && number_25 % 3 === 0) {
+    console.log(6)
+}
+
+else {
+    console.log(1)
+}
+
+alert ("38-vazifa")
+// 38 chi vazifa
+let number_26 = +prompt ("son-1")
+
+if (number_26 > 0 && number_26 % 7 === 0) {
+    console.log(14)
+}
+
+alert ("39-vazifa")
+// 39 chi vazifa
+let number_27 = +prompt ("son-1")
+
+if (number_27 > 0 || number_27 === 0) {
+    console.log(0)
+}
+
+else {
+    console.log(5)
+}
+
+alert ("40-vazifa")
+// 40 chi vazifa
+let number_28 = +prompt ("son-1")
+
+if (number_28 < 0 && number_28 % 10 !== 0) {
+    console.log(-3)
+}
---
+++ main.js
@@ -0,0 +1,200 @@
+// 1 chi vazifa
+let num = +prompt ("son kiriting-1")
+
+if (num > 0) {
+    console.log(1);
+}
+
+else {
+    console.log("berilgan son manfiy")
+}
+
+// 2 chi vazifa
+let number = +prompt ("son kiriting-2")
+
+if (number < 0) {
+    console.log(-1)
+}
+
+else {
+    console.log(0)
+}
+
+// 3 chi vazifa
+let number_1 = +prompt ("son kiriting-3")
+
+if (number_1 === 0) {
+    console.log(1)
+}
+
+else {
+    console.log(0)
+}
+
+// 4 chi vazifa
+let number_2 = +prompt ("son kiriting-4")
+
+if (number_2 > 100) {
+    console.log(1)
+}
+
+if (number_2 > 0) {
+    console.log(1)
+}
+
+// 5 chi vazifa
+let number_3 = +prompt ("son kiriting-5")
+
+if (number_3 < 0) {
+    console.log(-1)
+}
+
+if (number_3 < 10) {
+    console.log(-1)
+}
+// 6 chi vazifa
+let number_4 = +prompt ("son kiriting-6")
+
+if (number_4 > 0) {
+    console.log(number_4 * number_4)
+}
+
+else {
+    console.log(0)
+}
+
+// 7 chi vazifa
+let number_5 = +prompt ("son kiriting-7")
+
+if (number_5 < 0) {
+    console.log(-number_5)
+}
+
+else {
+    console.log(number_5)
+}
+
+// 8 chi vazifa
+let number_6 = +prompt ("son kiriting-8")
+
+if (number_6 === 0) {
+    console.log(10)
+}
+
+else {
+    console.log(number_6)
+}
+
+// 9 chi vazifa
+let number_7 = +prompt ("son kiriting-9")
+
+if (number_7 > 1) {
+    console.log(number_7 ** 3)
+}
+
+// 10 chi vazifa
+let number_8 = +prompt ("son kiriting-10")
+
+if (number_8 < 0) {
+    console.log(-10)
+}
+
+else {
+    console.log(10)
+}
+
+// juft va toq sonlar
+// 11 chi vazifa
+let number_9 = +prompt ("son kiriting-11")
+
+if (number_9 % 2 == 0) {
+    console.log(2)
+}
+
+else {
+    console.log(1)
+}
+
+// 12 chi vazifa
+let number_10 = +prompt ("son kiriting-12")
+
+if (number_10 % 2 == 1) {
+    console.log(3)
+}
+
+else {
+    console.log(4)
+}
+
+// 13 chi vazifa
+let number_11 = +prompt ("son kiriting-13")
+
+if (number_11 % 2 == 0) {
+    if ( number_11 > 10) {
+        console.log(1)
+    }
+}
+
+// 14 chi vazifa
+let number_12 = +prompt ("son kiriting-14")
+
+if (number_12 < 0) {
+    if (number_12 % -2 == -1) {
+        console.log(-1)
+    }
+}
+
+// 15 chi vazifa
+let number_13 = +prompt ("son kiriting-15")
+
+if (number_13 % 2 == 0) {
+    if (number_13 /= 5 == 0) {
+        console.log(100)
+    }
+}
+
+// 16 chi vazifa
+let number_14 = +prompt ("son kiriting-16")
+
+if (number_14 % 2 == 1) {
+    console.log(number_14 * number_14)
+}
+
+// 17 chi vazifa
+let number_15 = +prompt ("son kiriting-17")
+
+if (number_15 % 2 == 0) {
+    if (number_15 > 0) {
+        console.log(number_15 ** 3)
+    }
+}
+
+// 18 chi vazifa
+let number_16 = +prompt ("son kiriting-18")
+
+if (number_16 % 2 == 0) {
+    if (number_16 > 20) {
+        console.log(50)
+    }
+}
+else {
+    console.log(-50)
+}
+
+// 19 chi vazifa
+let number_17 = +prompt ("son kiriting-19")
+
+if (number_17 % 2 == 1) {
+    if (number_17 /= 7 == 0) {
+        console.log(7)
+    }
+}
+
+// 20 chi vazifa
+let number_18 = +prompt ("son kiriting-20")
+
+if (number_18 < 0) {
+    if (number_18 % 2 == 0) {
+        console.log(-20)
+    }
+}
